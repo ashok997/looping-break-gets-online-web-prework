1115ee54c5671d0ae76c5c0dd0b7a9197e5398be
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-07 01:46:05 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,19 @@
 
 def levitation_quiz
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end
 	#your code here
 end
 
 
+def annoying
+    loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
+end
