aa200cb6b995de3aa50321e5cd5d64e0e2f508ef
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-07 01:48:15 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -9,11 +9,3 @@ def levitation_quiz
 end
 
 
-#def annoying
-  #  loop do 
-     #   puts "What's up, Doc?"
-    #    answer = gets.chomp
-   #     break if answer == "STOP"
-  #  end
- #   puts "Okay, okay, jeez. I'll stop. Sorry."
-#end
